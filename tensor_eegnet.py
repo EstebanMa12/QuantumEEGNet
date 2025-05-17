8c2bf5b3ec36aec7864bf5c2f89360307c0e2e63
EstebanMa12 <desteban.maya@udea.edu.co>
2025-05-16 19:46:12 -0500

    Libraries update: I already installed the tensornetwork librery

--- tensor_eegnet.py
+++ tensor_eegnet.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 from scipy.linalg import svd
+import tensornetwork as tn 
 
 class TensorLayer(nn.Module):
     def __init__(self, input_dim, bond_dim, output_dim):
